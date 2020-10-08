b9c21f94bf8954255d951f9bb9704db4118d0eb1
Manuel Ponton <ma.pontonrico@zabud.com.co>
2020-10-07 19:36:06 -0500

    Avance

--- src/main/java/com/universidad/automatas/app/AutomatasController.java
+++ src/main/java/com/universidad/automatas/app/AutomatasController.java
@@ -1,9 +1,10 @@
 package com.universidad.automatas.app;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin("*")
 @RestController
 public class AutomatasController {
 
