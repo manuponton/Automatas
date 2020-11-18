abe19d9d496d43c653072943b25026cf2226b96a
Manuel Ponton <ma.pontonrico@zabud.com.co>
2020-11-17 22:22:46 -0500

    Se añade Swagger y se añaden validaciones

---
+++ src/main/java/com/universidad/automatas/app/NewText.java
@@ -0,0 +1,16 @@
+package com.universidad.automatas.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class NewText {
+
+  private String categoria;
+  private String text;
+}
